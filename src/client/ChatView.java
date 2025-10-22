7f32c94b8987d95a64ff2e255de4ce7f9c595368
Jago128 <jagobaetxegarai@gmail.com>
2025-10-22 14:23:54 +0200

    Continued work on client side.

--- src/client/ChatView.java
+++ src/client/ChatView.java
@@ -2,23 +2,28 @@ package client;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.*;
 import javax.swing.border.*;
 
 import model.Usuario;
+import threads.ClientThread;
 
 public class ChatView extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private Usuario user;
+	private Usuario user, user2;
 	private JLabel lblUsername, lblSetIP, lblSetPuerto;
 	private JTextField textFieldMessage, textFieldNewUser, textFieldSetPuerto, textFieldSetIP;
 	private JComboBox<String> comboBoxUsers;
 	private JButton btnSendMessage, btnDisconnect, btnCreateUser;
 	private JScrollPane scrollPaneMessages;
 	private JLabel lblMessage;
+	private Socket client;
 
 	public ChatView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,12 +36,12 @@ public class ChatView extends JFrame implements ActionListener {
 		btnDisconnect = new JButton("Disconnect");
 		btnDisconnect.setBounds(495, 3, 111, 27);
 		contentPane.add(btnDisconnect);
-		
+
 		lblUsername = new JLabel("Username:");
 		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblUsername.setBounds(27, 10, 111, 15);
 		contentPane.add(lblUsername);
-		
+
 		textFieldNewUser = new JTextField();
 		textFieldNewUser.setColumns(10);
 		textFieldNewUser.setBounds(27, 35, 458, 35);
@@ -45,17 +50,17 @@ public class ChatView extends JFrame implements ActionListener {
 		btnCreateUser = new JButton("Create User");
 		btnCreateUser.setBounds(495, 34, 111, 36);
 		contentPane.add(btnCreateUser);
-		
+
 		comboBoxUsers = new JComboBox<>();
 		comboBoxUsers.setEnabled(false);
 		comboBoxUsers.setBounds(27, 80, 256, 27);
 		contentPane.add(comboBoxUsers);
-		
+
 		lblSetIP = new JLabel("IP:");
 		lblSetIP.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblSetIP.setBounds(293, 85, 15, 15);
 		contentPane.add(lblSetIP);
-		
+
 		textFieldSetIP = new JTextField();
 		textFieldSetIP.setColumns(10);
 		textFieldSetIP.setBounds(315, 80, 141, 27);
@@ -65,28 +70,28 @@ public class ChatView extends JFrame implements ActionListener {
 		lblSetPuerto.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblSetPuerto.setBounds(466, 85, 49, 15);
 		contentPane.add(lblSetPuerto);
-		
+
 		textFieldSetPuerto = new JTextField();
 		textFieldSetPuerto.setColumns(10);
 		textFieldSetPuerto.setBounds(526, 80, 80, 27);
 		contentPane.add(textFieldSetPuerto);
-		
+
 		scrollPaneMessages = new JScrollPane();
 		scrollPaneMessages.setEnabled(false);
 		scrollPaneMessages.setBounds(27, 127, 579, 197);
 		contentPane.add(scrollPaneMessages);
-		
+
 		lblMessage = new JLabel("Message:");
 		lblMessage.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblMessage.setBounds(27, 334, 111, 15);
 		contentPane.add(lblMessage);
-		
+
 		textFieldMessage = new JTextField();
 		textFieldMessage.setEnabled(false);
 		textFieldMessage.setBounds(27, 359, 458, 35);
 		contentPane.add(textFieldMessage);
 		textFieldMessage.setColumns(10);
-		
+
 		btnSendMessage = new JButton("Send Message");
 		btnSendMessage.setEnabled(false);
 		btnSendMessage.setBounds(495, 358, 111, 36);
@@ -112,6 +117,44 @@ public class ChatView extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnCreateUser) {
+			createUser();
+		} else if (e.getSource() == btnSendMessage) {
+			sendMessage();
+		} else if (e.getSource() == btnDisconnect) {
+			
+		}
+	}
 
+	public void createUser() {
+		if (ipPortCheck()) {
+			user = new Usuario(textFieldNewUser.getText());
+		}
+	}
+
+	public boolean ipPortCheck() {
+		boolean check = true;
+		Pattern ip = Pattern.compile(
+				"^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$"),
+				port = Pattern.compile(
+						"^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$");
+		Matcher ipMatcher = ip.matcher(textFieldSetIP.getText());
+		if (!ipMatcher.matches()) {
+			check = false;
+		}
+
+		Matcher portMatcher = port.matcher(textFieldSetIP.getText());
+		if (!portMatcher.matches()) {
+			check = false;
+		}
+		return check;
+	}
+	
+	public void sendMessage() {
+		if (!comboBoxUsers.getSelectedItem().equals("Public")) {
+			ClientThread thread = new ClientThread(user, textFieldMessage.getText(), client);
+		} else {
+			ClientThread thread = new ClientThread(user, textFieldMessage.getText(), client);
+		}
 	}
 }
--- src/threads/ClientThread.java
+++ src/threads/ClientThread.java
@@ -1,13 +1,57 @@
 package threads;
 
+import java.net.Socket;
+
+import model.Usuario;
+
 public class ClientThread implements Runnable {
 
+	private Usuario user1;
+	private Usuario user2;
+	private String message;
+	private boolean sent;
+	private Socket client;
+	public Thread threadClient;
+
+	public void start() {
+		threadClient = new Thread(this, "Client");
+        threadClient.start();
+	}
+	
 	@Override
 	public void run() {
+		if (user2 != null) {
+			publicChat(user1, message);
+		} else {
+			privateChat(user1, user2, message);
+		}
+	}
+	
+	synchronized void publicChat(Usuario user, String message) {
+		
+		if (sent) {
+			
+		} else {
+			
+		}
+	}
+	
+	synchronized void privateChat(Usuario user1, Usuario user2, String message) {
 		
 	}
 	
-	synchronized void privateChat() {
-		
+	public ClientThread(Usuario user, String message, Socket client) {
+		this.user1 = user;
+		this.message = message;
+		this.sent = false;
+		this.client = client;
+	}
+
+	public ClientThread(Usuario user1, Usuario user2, String message, Socket client) {
+		this.user1 = user1;
+		this.user2 = user2;
+		this.message = message;
+		this.sent = false;
+		this.client = client;
 	}
 }
